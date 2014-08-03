c9ae6346dbc85e1635d3a7ecae945434f74b687b
luigi1015 <github@314ghz.com>
2014-08-03 18:39:32 -0500

    First commit.

---
+++ Random/src/net/codehobby/Random.java
@@ -0,0 +1,17 @@
+
+package net.codehobby;
+
+/**
+ *
+ * @author Jeff Crone
+ */
+public class Random {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
